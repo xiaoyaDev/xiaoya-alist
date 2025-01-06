d27e77c4808b50b2762232ed7ae4ab9f63ced635
DDSRem <73049927+DDSRem@users.noreply.github.com>
2025-01-06 16:01:44 +0800

    fix: sh-checker supported errors

--- all_in_one.sh
+++ all_in_one.sh
@@ -2111,7 +2111,7 @@ function unzip_appoint_xiaoya_emby_jellyfin() {
         if ! check_metadata_size "${1}"; then
             exit 1
         fi
-        if [[ "${OSNAME}" = "macos" ]]  || command -v 7z > /dev/null 2>&1; then
+        if [[ "${OSNAME}" = "macos" ]] || command -v 7z > /dev/null 2>&1; then
             INFO "使用宿主机 7z 命令解压"
             if [ ! -d "${MEDIA_DIR}/xiaoya" ]; then
                 mkdir -p "${MEDIA_DIR}/xiaoya"
