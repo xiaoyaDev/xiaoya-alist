68a307ee602eed607d2f6ca1d8c52032b9865b02
DDSDerek <108336573+DDSDerek@users.noreply.github.com>
2025-09-18 02:27:25 +0000

    chore: update embyserver latest version

--- all_in_one.sh
+++ all_in_one.sh
@@ -27,7 +27,7 @@ DATE_VERSION="v1.8.4-2025_06_14_19_04"
 amilys_embyserver_latest_version=4.8.11.0
 emby_embyserver_latest_version=4.8.11.0
 amilys_embyserver_beta_version=4.9.1.21
-emby_embyserver_beta_version=4.9.1.32
+emby_embyserver_beta_version=4.9.1.33
 # ——————————————————————————————————————————————————————————————————————————————————
 
 Sky_Blue="\033[36m"
